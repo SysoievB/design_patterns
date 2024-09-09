7bbd9f5e227c3fcabbc0ef8ff3c4811c247f158a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-09 09:15:05 +0200

    Added callback

--- other/callback/src/main/java/com/callback/Main.java
+++ other/callback/src/main/java/com/callback/Main.java
@@ -2,5 +2,27 @@ package com.callback;
 
 public class Main {
     public static void main(String[] args) {
+        Button button = new Button();
+        button.onClick(new ClickHandler());
+
+        Button lambdaButton = new Button();
+        lambdaButton.onClick(() -> System.out.println("Lambda"));
+    }
+}
+
+interface ClickEventHandler {
+    void handleClick();
+}
+
+class ClickHandler implements ClickEventHandler {
+    @Override
+    public void handleClick() {
+        System.out.println("Clicked");
+    }
+}
+
+class Button {
+    void onClick(ClickEventHandler handler) {
+        handler.handleClick();
     }
 }
