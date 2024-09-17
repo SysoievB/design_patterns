7c977fde80721102ac2c6352e99e8e503da997b1
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-17 08:12:46 +0200

    Added proxy

--- structural/proxy/src/main/java/com/proxy/Main.java
+++ structural/proxy/src/main/java/com/proxy/Main.java
@@ -1,7 +1,53 @@
 package com.proxy;
 
+import lombok.val;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        val image = new ProxyImage("image.jpg");
+        //Image will be loaded from disc only when display() is called
+        image.display();
+        //Image will not be loaded again, as it has been cached in the proxy
+        image.display();
+    }
+}
+
+//Subject
+interface Image {
+    void display();
+}
+
+//RealSubject
+class RealImage implements Image {
+    private String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+    }
+
+    private void loadImageFromDisc() {
+        System.out.println("Loading image: " + filename);
+    }
+
+    public void display() {
+        System.out.println("Displaying realImage: " + filename);
+    }
+}
+
+//Proxy
+class ProxyImage implements Image {
+    private RealImage realImage;
+    private String filename;
+
+    public ProxyImage(String filename) {
+        this.filename = filename;
+    }
+
+    @Override
+    public void display() {
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+        }
+        realImage.display();
     }
 }
