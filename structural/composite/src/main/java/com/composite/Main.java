0a78af99766eb9d47df91c19ee6b02e922de3c09
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-19 12:35:21 +0200

    Added composite

--- structural/composite/src/main/java/com/composite/Main.java
+++ structural/composite/src/main/java/com/composite/Main.java
@@ -4,3 +4,37 @@ public class Main {
     public static void main(String[] args) {
     }
 }
+
+// Component
+interface Task {
+    String getTitle();
+
+    void setTitle(String title);
+
+    void display();
+}
+
+// Leaf
+class SimpleTask implements Task {
+    private String title;
+
+    public SimpleTask(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Simple Task: " + title);
+    }
+}
+
