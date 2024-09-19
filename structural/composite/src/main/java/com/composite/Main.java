488bcd6bd0e7bdd4c69159198fffd59b725fb118
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-19 12:39:39 +0200

    Added composite

--- structural/composite/src/main/java/com/composite/Main.java
+++ structural/composite/src/main/java/com/composite/Main.java
@@ -1,7 +1,32 @@
 package com.composite;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        // Creating simple tasks
+        Task simpleTask1 = new SimpleTask("Complete Coding");
+        Task simpleTask2 = new SimpleTask("Write Documentation");
+
+        // Creating a task list
+        TaskList projectTasks = new TaskList("Project Tasks");
+        projectTasks.addTask(simpleTask1);
+        projectTasks.addTask(simpleTask2);
+
+        // Nested task list
+        TaskList phase1Tasks = new TaskList("Phase 1 Tasks");
+        phase1Tasks.addTask(new SimpleTask("Design"));
+        phase1Tasks.addTask(new SimpleTask("Implementation"));
+
+        projectTasks.addTask(phase1Tasks);
+
+        // Displaying tasks
+        projectTasks.display();
     }
 }
 
@@ -15,22 +40,11 @@ interface Task {
 }
 
 // Leaf
+@Getter
+@Setter
+@AllArgsConstructor
 class SimpleTask implements Task {
     private String title;
-
-    public SimpleTask(String title) {
-        this.title = title;
-    }
-
-    @Override
-    public String getTitle() {
-        return title;
-    }
-
-    @Override
-    public void setTitle(String title) {
-        this.title = title;
-    }
 
     @Override
     public void display() {
@@ -38,3 +52,31 @@ class SimpleTask implements Task {
     }
 }
 
+// Composite
+@Getter
+@Setter
+class TaskList implements Task {
+    private String title;
+    private List<Task> tasks;
+
+    public TaskList(String title) {
+        this.title = title;
+        this.tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(Task task) {
+        tasks.remove(task);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Task List: " + title);
+        for (Task task : tasks) {
+            task.display();
+        }
+    }
+}
