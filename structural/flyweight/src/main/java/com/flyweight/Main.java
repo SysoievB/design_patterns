ec80c367a9db168f30e0a5842a99118aa81bdcfb
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-20 13:00:26 +0200

    Added flyweight

--- structural/flyweight/src/main/java/com/flyweight/Main.java
+++ structural/flyweight/src/main/java/com/flyweight/Main.java
@@ -4,3 +4,8 @@ public class Main {
     public static void main(String[] args) {
     }
 }
+
+// Flyweight interface
+interface Icon {
+    void draw(int x, int y);  // Method to draw the icon at given coordinates
+}
