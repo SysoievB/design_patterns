a00423d1d4a15ec630308b8dfc206d563530a94c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-21 07:46:20 +0200

    Added chain of responsisbility

--- behavioral/chain_of_responsisbility/src/main/java/com/chain_of_responsisbility/Main.java
+++ behavioral/chain_of_responsisbility/src/main/java/com/chain_of_responsisbility/Main.java
@@ -1,7 +1,10 @@
 package com.chain_of_responsisbility;
-
+/**
+ * The Chain of Responsibility design pattern is a behavioral design pattern that
+ * allows an object to pass a request along a chain of handlers. Each handler in the
+ * chain decides either to process the request or to pass it along the chain to the
+ * next handler.*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
