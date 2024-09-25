1ca1ff4efba4511f8776ca53aebc2ed0569abe8f
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-09-25 05:47:50 +0200

    Added mediator

---
+++ behavioral/mediator/src/main/java/com/mediator/Main.java
@@ -0,0 +1,10 @@
+package com.mediator;
+/**
+ * Mediator is a behavioral design pattern that lets you reduce chaotic dependencies
+ * between objects. The pattern restricts direct communications between the objects
+ * and forces them to collaborate only via a mediator object.*/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
