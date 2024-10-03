4c132cbe86a5cfb748102d8f69101eae672b29ff
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-03 17:11:42 +0200

    Added strategy

---
+++ behavioral/strategy/src/main/java/com/strategy/Main.java
@@ -0,0 +1,10 @@
+package com.strategy;
+/**
+ * The Strategy Design Pattern defines a family of algorithms, encapsulates each one,
+ * and makes them interchangeable, allowing clients to switch algorithms dynamically
+ * without altering the code structure.*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
