6703d836ee8faa0cbc8099da1e0db7d2060b7a5d
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-03 17:25:22 +0200

    Added template

--- behavioral/template/src/main/java/com/template/Main.java
+++ behavioral/template/src/main/java/com/template/Main.java
@@ -1,11 +1,72 @@
 package com.template;
+
 /**
  * Template Method is a behavioral design pattern that allows you to define a skeleton
  * of an algorithm in a base class and let subclasses override the steps without
  * changing the overall algorithm’s structure.
  * Typical Abstract class.
- * */
+ */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Making tea:");
+        BeverageMaker teaMaker = new TeaMaker();
+        teaMaker.makeBeverage();
+
+        System.out.println("\nMaking coffee:");
+        BeverageMaker coffeeMaker = new CoffeeMaker();
+        coffeeMaker.makeBeverage();
+    }
+}
+
+// Abstract class defining the template method
+abstract class BeverageMaker {
+    // Template method defining the overall process
+    public final void makeBeverage() {
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    // Abstract methods to be implemented by subclasses
+    abstract void brew();
+
+    abstract void addCondiments();
+
+    // Common methods
+    void boilWater() {
+        System.out.println("Boiling water");
+    }
+
+    void pourInCup() {
+        System.out.println("Pouring into cup");
+    }
+}
+
+// Concrete subclass for making tea
+class TeaMaker extends BeverageMaker {
+    // Implementing abstract methods
+    @Override
+    void brew() {
+        System.out.println("Steeping the tea");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("Adding lemon");
+    }
+}
+
+// Concrete subclass for making coffee
+class CoffeeMaker extends BeverageMaker {
+    // Implementing abstract methods
+    @Override
+    void brew() {
+        System.out.println("Dripping coffee through filter");
+    }
+
+    @Override
+    void addCondiments() {
+        System.out.println("Adding sugar and milk");
     }
 }
