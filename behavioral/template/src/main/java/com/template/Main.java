da3bc73a5cce60381b2268afb1b1bb3b6e9da57c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-10-03 17:22:09 +0200

    Added template

---
+++ behavioral/template/src/main/java/com/template/Main.java
@@ -0,0 +1,11 @@
+package com.template;
+/**
+ * Template Method is a behavioral design pattern that allows you to define a skeleton
+ * of an algorithm in a base class and let subclasses override the steps without
+ * changing the overall algorithm’s structure.
+ * Typical Abstract class.
+ * */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
