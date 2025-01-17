3cba2a3044756a8eab9287cb041eaa865963ada3
SysoievB <sysoiev.bogdan.ua@gmail.com>
2025-01-17 10:34:12 +0100

    Added sealed interface & records

--- other/sealed_interface_and_records/src/main/java/com/Main.java
+++ other/sealed_interface_and_records/src/main/java/com/Main.java
@@ -2,6 +2,24 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Car car = new Car("BMW");
+        System.out.println(car.commonMethod());
+    }
+}
+
+sealed interface CommonSealedInterface permits Car, Truck{
+    String commonMethod();
+}
+
+record Car(String param) implements CommonSealedInterface {
+    @Override
+    public String commonMethod() {
+        return param;
+    }
+}
+record Truck(String param) implements CommonSealedInterface {
+    @Override
+    public String commonMethod() {
+        return param;
     }
 }
